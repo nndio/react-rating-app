7332c4d74d1f75ff9ddb25aae7c7f5493fe99a7c
Anastasia <anastasia.diordiev@gmail.com>
2026-08-21 08:41:47 +0300

    Delete Routing directory

--- Routing/homework-starter/src/pages/PlaylistInfoPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { PlaylistInfoPage } from "./PlaylistInfoPage";
--- Routing/homework-starter/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { MainPage } from "./MainPage";
-export { UsersPage } from "./UsersPage";
-export { UserInfoPage } from "./UserInfoPage";
-export { PlaylistsPage } from "./PlaylistsPage";
-export { PlaylistInfoPage } from "./PlaylistInfoPage";
